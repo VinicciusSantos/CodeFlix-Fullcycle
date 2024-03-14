55cc270d33b85b1312710f50b3205e991a60482f
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2024-03-14 09:49:55 -0300

    :sparkles: feat: create category entity

---
+++ src/domain/category.entity.ts
@@ -0,0 +1,59 @@
+export interface CategoryProps {
+  category_id?: string;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
+}
+
+export interface CategoryCreateCommand {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+}
+
+export class Category {
+  public readonly category_id: string;
+  public name: string;
+  public description: string | null;
+  public is_active: boolean;
+  public readonly created_at: Date;
+
+  constructor(props: CategoryProps) {
+    this.category_id = props.category_id ?? 'default';
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
+  }
+
+  public static create(props: CategoryCreateCommand): Category {
+    return new Category(props);
+  }
+
+  public changeName(name: string): void {
+    this.name = name;
+  }
+
+  public changeDescription(description: string): void {
+    this.description = description;
+  }
+
+  public activate(): void {
+    this.is_active = true;
+  }
+
+  public deactivate(): void {
+    this.is_active = false;
+  }
+
+  public toJSON(): CategoryProps {
+    return {
+      category_id: this.category_id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at,
+    };
+  }
+}
