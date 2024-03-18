e903ad3df4d32a5ea29c45a04c1badd07196282c
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2024-03-18 08:20:23 -0300

    :test_tube: test: fixing database test

--- src/nest-modules/database-module/__tests__/database.module.spec.ts
+++ src/nest-modules/database-module/__tests__/database.module.spec.ts
@@ -35,43 +35,4 @@ describe('DatabaseModule Unit Tests', () => {
       await conn.close();
     });
   });
-
-  describe('mysql connection', () => {
-    const connOptions = {
-      DB_VENDOR: 'mysql',
-      DB_HOST: 'db',
-      DB_DATABASE: 'micro_videos',
-      DB_USERNAME: 'root',
-      DB_PASSWORD: 'root',
-      DB_PORT: 3306,
-      DB_LOGGING: false,
-      DB_AUTO_LOAD_MODELS: true,
-    };
-
-    it('should be a mysql connection', async () => {
-      const module = await Test.createTestingModule({
-        imports: [
-          DatabaseModule,
-          ConfigModule.forRoot({
-            isGlobal: true,
-            ignoreEnvFile: true,
-            ignoreEnvVars: true,
-            validationSchema: null,
-            load: [() => connOptions],
-          }),
-        ],
-      }).compile();
-
-      const app = module.createNestApplication();
-      const conn = app.get<Sequelize>(getConnectionToken());
-      expect(conn).toBeDefined();
-      expect(conn.options.dialect).toBe(connOptions.DB_VENDOR);
-      expect(conn.options.host).toBe(connOptions.DB_HOST);
-      expect(conn.options.database).toBe(connOptions.DB_DATABASE);
-      expect(conn.options.username).toBe(connOptions.DB_USERNAME);
-      expect(conn.options.password).toBe(connOptions.DB_PASSWORD);
-      expect(conn.options.port).toBe(connOptions.DB_PORT);
-      await conn.close();
-    });
-  });
 });
